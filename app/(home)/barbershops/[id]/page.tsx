eec53b56aaf9154bc40409bf4d748ecfba174973
Elionai Melo <elionai.melo@gmail.com>
2024-02-03 16:58:33 -0300

    feat: Add service items to barbershop details page

--- app/(home)/barbershops/[id]/page.tsx
+++ app/(home)/barbershops/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 
 import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
 
 interface barbershopDetailsPageProps {
   params: {
@@ -18,11 +19,24 @@ const barbershopDetailsPage = async ({
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   });
 
   if (!barbershop) return <h1>Barbershop not found</h1>;
 
-  return <BarbershopInfo barbershop={barbershop} />;
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+
+      <div className="px-5 flex flex-col gap-5 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default barbershopDetailsPage;
