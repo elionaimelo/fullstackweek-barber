d696ca3c3e76e8d7b05d144cdaacd5ec8008123f
Elionai Melo <elionai.melo@gmail.com>
2024-02-03 15:49:16 -0300

    refactor :barbershopDetailsPage component

--- app/(home)/barbershops/[id]/page.tsx
+++ app/(home)/barbershops/[id]/page.tsx
@@ -1,28 +1,28 @@
-import { db } from "@/app/_lib/prisma"
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+
+import BarbershopInfo from "./_components/barbershop-info";
 
 interface barbershopDetailsPageProps {
   params: {
-    id?: string
-  }
+    id?: string;
+  };
 }
 
-const barbershopDetailsPage = async ({ params}: barbershopDetailsPageProps) => {
-  
-  if(!params.id) return (<h1>Barbershop not found</h1>)
+const barbershopDetailsPage = async ({
+  params,
+}: barbershopDetailsPageProps) => {
+  if (!params.id) return <h1>Barbershop not found</h1>;
 
   const barbershop = await db.barbershop.findUnique({
     where: {
-      id: params.id
-    }
-  })
+      id: params.id,
+    },
+  });
 
-  if(!barbershop) return (<h1>Barbershop not found</h1>)
+  if (!barbershop) return <h1>Barbershop not found</h1>;
 
-  return (
-    <div>
-      <h1>{barbershop?.name}</h1>
-    </div>
-  )
-}
+  return <BarbershopInfo barbershop={barbershop} />;
+};
 
-export default barbershopDetailsPage
+export default barbershopDetailsPage;
